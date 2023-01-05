9eb94a045f692e38cce4aecc6eee666db7892fbd
Wappen <lenz.koxholt@gmail.com>
2023-01-05 19:27:48 +0100

    Make game set its own working dir to use relative assets folder

--- examples/test_game/main.rs
+++ examples/test_game/main.rs
@@ -1,9 +1,16 @@
 use cooltraption_playground::asset_manager::file_asset_manager::FileAssetManager;
 use cooltraption_playground::asset_manager::{Asset, AssetManager};
+use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let manager = FileAssetManager::load(PathBuf::from("target/debug/assets"));
+    set_working_dir().expect("Could not set working dir");
+    println!(
+        "Starting in {}",
+        env::current_dir().unwrap().to_str().unwrap()
+    );
+
+    let manager = FileAssetManager::load(PathBuf::from("./assets"));
     match manager.get_asset("strings.yml").unwrap() {
         Asset::Strings(map) => {
             println!("{}", map.get("greet").unwrap());
@@ -12,4 +19,21 @@ fn main() {
             println!("Didn't find Strings asset");
         }
     }
+}
+
+#[derive(Debug)]
+enum Error {
+    GetError,
+    SetError,
+}
+
+fn set_working_dir() -> Result<(), Error> {
+    let mut working_dir = env::current_exe().or(Err(Error::GetError))?;
+    Some(working_dir.pop())
+        .filter(|_| true)
+        .ok_or(Error::GetError)?;
+    Some(working_dir.pop())
+        .filter(|_| true)
+        .ok_or(Error::GetError)?;
+    env::set_current_dir(working_dir).or(Err(Error::SetError))
 }
