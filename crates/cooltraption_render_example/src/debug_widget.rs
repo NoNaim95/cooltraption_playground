1d119511e55a51f11968a8718981e6957bf44b6b
Wappen <lenz.koxholt@gmail.com>
2023-05-02 16:33:11 +0200

    Display all fps stats (min, max, avg)

--- crates/cooltraption_render_example/src/debug_widget.rs
+++ crates/cooltraption_render_example/src/debug_widget.rs
@@ -2,22 +2,37 @@ use cooltraption_render::gui::{egui, Widget, WidgetId};
 use cooltraption_window::events::EventHandler;
 use cooltraption_window::window::winit::dpi::PhysicalSize;
 use cooltraption_window::window::{WindowContext, WinitEvent};
+use std::fmt::{Display, Formatter};
 use std::time::Instant;
 
 struct FpsCounter {
-    min_fps: f32,
-    max_fps: f32,
-    avg_fps: f32,
+    min: f32,
+    max: f32,
+    avg: f32,
     frame_count: u32,
     start_time: Instant,
+}
+
+impl Display for FpsCounter {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        if self.min == f32::MAX {
+            write!(f, "(MIN / MAX / AVG): - / - / -")
+        } else {
+            write!(
+                f,
+                "(MIN / MAX / AVG): {:.0} / {:.0} / {:.0}",
+                self.min, self.max, self.avg
+            )
+        }
+    }
 }
 
 impl FpsCounter {
     fn new() -> Self {
         Self {
-            min_fps: f32::MAX,
-            max_fps: 0.0,
-            avg_fps: 0.0,
+            min: f32::MAX,
+            max: 0.0,
+            avg: 0.0,
             frame_count: 0,
             start_time: Instant::now(),
         }
@@ -28,14 +43,14 @@ impl FpsCounter {
         let elapsed_time = self.start_time.elapsed();
         if elapsed_time.as_secs() >= 1 {
             let fps = self.frame_count as f32 / elapsed_time.as_secs_f32();
-            if fps < self.min_fps {
-                self.min_fps = fps;
+            if fps < self.min {
+                self.min = fps;
             }
-            if fps > self.max_fps {
-                self.max_fps = fps;
+            if fps > self.max {
+                self.max = fps;
             }
-            self.avg_fps = (self.avg_fps * (elapsed_time.as_secs_f32() - 1.0) + fps)
-                / elapsed_time.as_secs_f32();
+            self.avg =
+                (self.avg * (elapsed_time.as_secs_f32() - 1.0) + fps) / elapsed_time.as_secs_f32();
             self.frame_count = 0;
             self.start_time = Instant::now();
         }
@@ -79,8 +94,11 @@ impl Widget for DebugWidget {
             .resizable(false)
             .show(context, |ui| {
                 ui.label(format!("{:?}", self.window_size));
-                ui.label(format!("FPS: {:.2}", self.fps.avg_fps));
-                ui.label(format!("TPS {:.2}", self.tps.avg_fps));
+
+                ui.add_space(10.0);
+
+                ui.label(format!("FPS {}", self.fps));
+                ui.label(format!("TPS {}", self.tps));
             });
 
         self.is_open
