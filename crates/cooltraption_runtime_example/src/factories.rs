7ee0d37974c11d2f4d5ba51a4b2ac9870b62509f
NoNaim95 <me@deni-ismailov.de>
2023-05-03 21:43:37 +0200

    move factory methods to factories

--- crates/cooltraption_runtime_example/src/factories.rs
+++ crates/cooltraption_runtime_example/src/factories.rs
@@ -1,10 +1,17 @@
+use cgmath::Vector2;
+use cooltraption_network as networking;
+use cooltraption_common::events::EventHandler;
+use cooltraption_input::input::{InputEvent, KeyboardInputEvent};
+use cooltraption_network::client;
+use cooltraption_render::world_renderer::{WorldState, world_state::{Drawable, Id, Scale, Rotation}};
 use cooltraption_simulation::{
-    action::{Action, SpawnBallAction},
-    system_sets::physics_set::{FromNum2, Vec2f},
-    Position,
+    action::{Action, SpawnBallAction, CircularForceAction, ActionPacket},
+    system_sets::physics_set::{FromNum2, Vec2f, Float},
+    Position, QueryIter, Entity,
 };
+use cooltraption_window::window::winit::event::VirtualKeyCode;
 use rand::random;
-use std::iter;
+use std::{iter, sync::mpsc::{Sender, SyncSender}, time::Duration};
 
 fn randomspawn_action(max_x: i32, max_y: i32) -> Action {
     let (x, y) = (random::<i32>() % max_x, random::<i32>() % max_y);
@@ -22,4 +29,73 @@ pub fn sometimes_spawn_action(max_x: i32, max_y: i32, n: i32) -> impl Iterator<I
         }
         None
     })
+}
+
+pub fn create_input_handler(input_action_sender: Sender<Action>) -> impl EventHandler<InputEvent> {
+    return move |input_event: &InputEvent| {
+        if let InputEvent::KeyboardInputEvent(keyboard_input_event) = input_event {
+            if let KeyboardInputEvent::KeyPressed(key_code, ..) = keyboard_input_event {
+                match key_code {
+                    VirtualKeyCode::Space => {
+                        let circular_force_action = CircularForceAction {
+                            position: Position(Vec2f::from_num(0, 0)),
+                            strength: Float::from_num(30),
+                        };
+                        input_action_sender
+                            .send(Action::CircularForce(circular_force_action))
+                            .unwrap();
+                    }
+                    VirtualKeyCode::E => {
+                        let spawn_ball_action = SpawnBallAction {
+                            position: Position(Vec2f::from_num(10, 10)),
+                        };
+                        input_action_sender
+                            .send(Action::SpawnBall(spawn_ball_action))
+                            .unwrap();
+                    }
+                    _ => (),
+                }
+            }
+        }
+    };
+}
+
+pub fn action_packet_from_server_iter() -> impl Iterator<Item = ActionPacket> {
+    let (_node_handler, mut event_receiver, _node_task, _server) =
+        client::Client::connect("127.0.0.1:5000".parse().unwrap(), Duration::from_secs(3))
+            .expect("could not connect from main");
+    std::iter::from_fn(move || event_receiver.try_receive()).map(|stored_event| match stored_event
+        .network()
+    {
+        networking::StoredNetEvent::Message(_, message) => {
+            serde_yaml::from_slice::<ActionPacket>(&message).unwrap()
+        }
+        networking::StoredNetEvent::Disconnected(_) => {
+            panic!("We got disconnected")
+        }
+        _ => unreachable!(),
+    })
+}
+
+pub fn sim_state_sender(
+    world_state_sender: SyncSender<WorldState>,
+) -> impl FnMut(QueryIter<'_, '_, (Entity, &Position), ()>) {
+    move |comp_iter: QueryIter<(Entity, &Position), ()>| {
+        let mut drawables = vec![];
+        for (entity, pos) in comp_iter {
+            let rpos = pos.0;
+            let mut pos: Vector2<f32> = Vector2::new(rpos.x.0.to_num(), rpos.y.0.to_num());
+            pos /= 100.0;
+            let drawable = Drawable {
+                id: Id(entity.index() as u64),
+                position: cooltraption_render::world_renderer::world_state::Position(pos),
+                scale: Scale(Vector2::new(1.0, 1.0)),
+                asset_name: String::from("dude"),
+                rot: Rotation::default(),
+            };
+            drawables.push(drawable);
+        }
+        let world_state = WorldState { drawables };
+        world_state_sender.send(world_state).unwrap();
+    }
 }
--- crates/cooltraption_runtime_example/src/main.rs
+++ crates/cooltraption_runtime_example/src/main.rs
@@ -1,25 +1,18 @@
-use cgmath::Vector2;
-use cooltraption_common::events::{EventHandler, EventPublisher, MutEventPublisher};
-use cooltraption_network as networking;
+use cooltraption_common::events::{EventPublisher, MutEventPublisher};
 use cooltraption_network::client;
 use cooltraption_network::network_state::NetworkStateEventHandler;
 use cooltraption_network::network_state_handler::NetworkStateHandler;
 use cooltraption_network::server::ServerNetworkingEngine;
-use cooltraption_render::world_renderer::world_state::{Drawable, Id, Rotation, Scale};
 use cooltraption_render::world_renderer::WorldState;
-use cooltraption_simulation::action::{
-    Action, ActionPacket, CircularForceAction, SpawnBallAction,
-};
-use cooltraption_simulation::system_sets::physics_set::{Float, FromNum2, Vec2f};
+use cooltraption_simulation::action::{Action, ActionPacket};
 use cooltraption_simulation::*;
-use cooltraption_window::window::winit::event::VirtualKeyCode;
 use directors::SimulationImplDirector;
 
 use std::iter;
 
-use cooltraption_input::input::{InputEvent, InputEventHandler, KeyboardInputEvent};
+use cooltraption_input::input::{InputEvent, InputEventHandler};
 
-use std::sync::mpsc::{self, SyncSender, Sender};
+use std::sync::mpsc::{self, SyncSender};
 use std::time::Duration;
 
 pub mod directors;
@@ -37,40 +30,12 @@ fn main() {
     });
 
     let mut event_publisher = EventPublisher::<InputEvent>::default();
-    event_publisher.add_event_handler(create_input_handler(input_action_sender));
+    event_publisher.add_event_handler(factories::create_input_handler(input_action_sender));
 
     let it = iter::from_fn(move || state_recv.try_recv().ok());
     render_component::run_renderer(it, InputEventHandler::new(event_publisher));
 }
 
-pub fn create_input_handler(input_action_sender: Sender<Action>) -> impl EventHandler<InputEvent>{
-    return move |input_event: &InputEvent| {
-        if let InputEvent::KeyboardInputEvent(keyboard_input_event) = input_event {
-            if let KeyboardInputEvent::KeyPressed(key_code, ..) = keyboard_input_event {
-                match key_code {
-                    VirtualKeyCode::Space => {
-                        let circular_force_action = CircularForceAction {
-                            position: Position(Vec2f::from_num(0, 0)),
-                            strength: Float::from_num(30),
-                        };
-                        input_action_sender
-                            .send(Action::CircularForce(circular_force_action))
-                            .unwrap();
-                    }
-                    VirtualKeyCode::E => {
-                        let spawn_ball_action = SpawnBallAction {
-                            position: Position(Vec2f::from_num(10, 10)),
-                        };
-                        input_action_sender
-                            .send(Action::SpawnBall(spawn_ball_action))
-                            .unwrap();
-                    }
-                    _ => (),
-                }
-            }
-        }
-    };
-}
 
 pub fn server_example() {
     let network_state_handler = NetworkStateHandler::new(3);
@@ -84,45 +49,7 @@ pub fn server_example() {
     ServerNetworkingEngine {}.run(5000, event_publisher);
 }
 
-pub fn action_packet_from_server_iter() -> impl Iterator<Item = ActionPacket> {
-    let (_node_handler, mut event_receiver, _node_task, _server) =
-        client::Client::connect("127.0.0.1:5000".parse().unwrap(), Duration::from_secs(3))
-            .expect("could not connect from main");
-    std::iter::from_fn(move || event_receiver.try_receive()).map(|stored_event| match stored_event
-        .network()
-    {
-        networking::StoredNetEvent::Message(_, message) => {
-            serde_yaml::from_slice::<ActionPacket>(&message).unwrap()
-        }
-        networking::StoredNetEvent::Disconnected(_) => {
-            panic!("We got disconnected")
-        }
-        _ => unreachable!(),
-    })
-}
 
-fn sim_state_sender(
-    world_state_sender: SyncSender<WorldState>,
-) -> impl FnMut(QueryIter<'_, '_, (Entity, &Position), ()>) {
-    move |comp_iter: QueryIter<(Entity, &Position), ()>| {
-        let mut drawables = vec![];
-        for (entity, pos) in comp_iter {
-            let rpos = pos.0;
-            let mut pos: Vector2<f32> = Vector2::new(rpos.x.0.to_num(), rpos.y.0.to_num());
-            pos /= 100.0;
-            let drawable = Drawable {
-                id: Id(entity.index() as u64),
-                position: cooltraption_render::world_renderer::world_state::Position(pos),
-                scale: Scale(Vector2::new(1.0, 1.0)),
-                asset_name: String::from("dude"),
-                rot: Rotation::default(),
-            };
-            drawables.push(drawable);
-        }
-        let world_state = WorldState { drawables };
-        world_state_sender.send(world_state).unwrap();
-    }
-}
 
 pub fn run_simulation<I, IP>(
     local_action_iterator: I,
@@ -137,7 +64,7 @@ pub fn run_simulation<I, IP>(
         .build()
         .unwrap();
 
-    sim.add_query_iter_handler(sim_state_sender(world_state_sender));
+    sim.add_query_iter_handler(factories::sim_state_sender(world_state_sender));
 
     //sim.add_local_action_handler(move |action_packet| {
     //    node_handler.network().send(
