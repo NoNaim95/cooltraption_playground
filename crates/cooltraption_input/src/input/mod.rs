98f1b1444597e63178957aa8a0441f4dbdfc446f
NoNaim95 <me@deni-ismailov.de>
2023-05-14 14:55:26 +0200

    Refactor Input to use new Event System

--- crates/cooltraption_input/src/input/mod.rs
+++ crates/cooltraption_input/src/input/mod.rs
@@ -1,5 +1,6 @@
 mod keyboard_state;
 mod mouse_state;
+mod events;
 
 use crate::input::keyboard_state::KeyboardState;
 use crate::input::mouse_state::MouseState;
@@ -11,8 +12,10 @@ use cooltraption_window::window::winit::event::{
 };
 use cooltraption_window::window::{winit, WindowContext, WinitEvent};
 
+use events::Event;
+
 pub struct InputEventHandler<'a> {
-    event_publisher: EventPublisher<'a, InputEvent>,
+    event_publisher: EventPublisher<'a, Event<'a, InputEvent, InputState>>,
     input_state: InputState,
 }
 
@@ -27,23 +30,23 @@ pub struct InputState {
 pub enum InputEvent {
     KeyboardInputEvent(KeyboardInputEvent),
     MouseButtonEvent(MouseButtonEvent),
-    MouseMoved(PhysicalPosition<f64>, InputState),
+    MouseMoved(PhysicalPosition<f64>),
 }
 
 #[derive(Debug)]
 pub enum KeyboardInputEvent {
-    KeyPressed(VirtualKeyCode, InputState),
-    KeyReleased(VirtualKeyCode, InputState),
+    KeyPressed(VirtualKeyCode),
+    KeyReleased(VirtualKeyCode),
 }
 
 #[derive(Debug)]
 pub enum MouseButtonEvent {
-    KeyPressed(MouseButton, InputState),
-    KeyReleased(MouseButton, InputState),
+    KeyPressed(MouseButton),
+    KeyReleased(MouseButton),
 }
 
 impl<'a> InputEventHandler<'a> {
-    pub fn new(event_publisher: EventPublisher<'a, InputEvent>) -> Self {
+    pub fn new(event_publisher: EventPublisher<'a, Event<'a, InputEvent, InputState>>) -> Self {
         Self {
             event_publisher,
             input_state: InputState::default(),
@@ -57,18 +60,16 @@ impl<'a> InputEventHandler<'a> {
                     self.input_state.keyboard_state.set_button(&key_code, true);
                     InputEvent::KeyboardInputEvent(KeyboardInputEvent::KeyPressed(
                         key_code,
-                        self.input_state.clone(),
                     ))
                 }
                 ElementState::Released => {
                     self.input_state.keyboard_state.set_button(&key_code, false);
                     InputEvent::KeyboardInputEvent(KeyboardInputEvent::KeyReleased(
                         key_code,
-                        self.input_state.clone(),
                     ))
                 }
             };
-            self.event_publisher.publish(&event);
+            self.event_publisher.publish(&Event::new(&event, &self.input_state));
         }
     }
 
@@ -78,24 +79,22 @@ impl<'a> InputEventHandler<'a> {
                 self.input_state.mouse_state.set_button(&button, true);
                 InputEvent::MouseButtonEvent(MouseButtonEvent::KeyPressed(
                     button,
-                    self.input_state.clone(),
                 ))
             }
             ElementState::Released => {
                 self.input_state.mouse_state.set_button(&button, false);
                 InputEvent::MouseButtonEvent(MouseButtonEvent::KeyReleased(
                     button,
-                    self.input_state.clone(),
                 ))
             }
         };
-        self.event_publisher.publish(&event);
+        self.event_publisher.publish(&Event::new(&event, &self.input_state));
     }
 
     fn mouse_moved(&mut self, pos: &mut PhysicalPosition<f64>) {
         self.input_state.mouse_state.set_mouse_position(*pos);
-        let event = InputEvent::MouseMoved(*pos, self.input_state.clone());
-        self.event_publisher.publish(&event);
+        let event = InputEvent::MouseMoved(*pos);
+        self.event_publisher.publish(&Event::new(&event, &self.input_state));
     }
 }
 
