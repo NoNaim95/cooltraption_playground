e74274594c257a217c20c44b685a09a460360be6
NoNaim95 <me@deni-ismailov.de>
2023-05-17 16:22:53 +0200

    use schedule plugin again

--- crates/cooltraption_runtime_example/src/main.rs
+++ crates/cooltraption_runtime_example/src/main.rs
@@ -4,6 +4,7 @@ use std::sync::mpsc::channel;
 
 use cooltraption_runtime::configurators::common_configurators::add_renderer;
 use cooltraption_runtime::configurators::{Configurator, ConfiguratorOnce, ConfiguratorPipeline};
+use cooltraption_runtime::factories::create_schedule;
 use cooltraption_runtime::{Runtime, RuntimeConfigurationBuilder};
 
 pub mod factories;
@@ -14,18 +15,18 @@ fn main() {
     let mut runtime_config_builder = RuntimeConfigurationBuilder::default();
     let mut configurator_pipeline = ConfiguratorPipeline::default();
 
-    let input_iter = Box::new(iter::from_fn(move || input_action_receiver.try_recv().ok()));
+    let input_action_iter = Box::new(iter::from_fn(move || input_action_receiver.try_recv().ok()));
     let input_action_configurator = for<'a> move |mut rt_config: RuntimeConfigurationBuilder<'a>| -> RuntimeConfigurationBuilder<'a>
     {
         rt_config
             .simulation_run_options_builder()
-            .set_actions(input_iter);
+            .set_actions(input_action_iter);
         rt_config
     };
 
     let add_schedule_configurator =
-        for<'a> |rt_config: RuntimeConfigurationBuilder<'a>| -> RuntimeConfigurationBuilder<'a> {
-            //rt_config.sim_builder.schedule(create_schedule());
+        for<'a> |mut rt_config: RuntimeConfigurationBuilder<'a>| -> RuntimeConfigurationBuilder<'a> {
+            rt_config.simulation_builder().set_schedule(create_schedule());
             rt_config
         };
 
