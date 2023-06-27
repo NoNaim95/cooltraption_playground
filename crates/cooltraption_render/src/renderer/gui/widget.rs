21d4702f4f6e5372581ec6e825dc43002c0cfde7
Wappen <lenz.koxholt@gmail.com>
2023-06-27 17:21:54 +0200

    Add more documentation

--- crates/cooltraption_render/src/renderer/gui/widget.rs
+++ crates/cooltraption_render/src/renderer/gui/widget.rs
@@ -3,6 +3,7 @@ use cooltraption_window::events::EventHandler;
 use cooltraption_window::window::{WindowContext, WinitEvent};
 use egui::Context;
 
+/// A widget is a GUI element that can be rendered by the GUI renderer
 pub trait Widget: for<'a, 'b, 'c> EventHandler<WinitEvent<'a, 'b>, WindowContext<'c>> {
     fn show(&mut self, context: &Context) -> bool;
     fn id(&self) -> WidgetId;
