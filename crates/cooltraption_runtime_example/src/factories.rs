302d970402e4754644303819b3807d7651b45525
NoNaim95 <me@deni-ismailov.de>
2023-07-01 12:55:04 +0200

    Fix signature to be a configurator

--- crates/cooltraption_runtime_example/src/factories.rs
+++ crates/cooltraption_runtime_example/src/factories.rs
@@ -3,7 +3,7 @@ use cooltraption_simulation::action::{Action, SpawnBallAction};
 use std::iter;
 
 #[allow(dead_code)]
-fn sometimes_spawn_ball(mut rt_config_builder: RuntimeConfigurationBuilder) {
+fn sometimes_spawn_ball(rt_config_builder: &mut RuntimeConfigurationBuilder) {
     let mut i = 0;
     let boxed_it = Box::new(iter::from_fn(move || {
         i += 1;
